1139331f3afcee040e8b8c5e3c476dfe9ead8ea6
Cristian Delcid <cristian_delcid10@yahoo.com>
2023-02-24 15:21:30 -0600

    Database changed to SQLite

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -79,12 +79,15 @@ WSGI_APPLICATION = 'moviereviews.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'moviereviews',
+    #     'USER': 'root',
+    #     'PASSWORD': 'password',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': 'moviereviews',
-        'USER': 'root',
-        'PASSWORD': 'password'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
